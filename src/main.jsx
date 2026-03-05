c49cde84e5707be58bdd80ce7a8fe7934e8ff66d
benedetz <benedetz@oregonstate.edu>
2026-03-04 16:57:33 -0800

    code structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomeLayout from './pages/HomeLayout'
+import Start from './pages/Start'
+import Levels from './pages/Levels'
+import Level from './pages/Level'
+import Shop from './pages/Shop'
+import Pokedex from './pages/Pokedex'
+import Stats from './pages/Stats'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  { path: '/', element: <Start /> },
+  {
+    path: '/home',
+    element: <HomeLayout />,
+    children: [
+      { index: true, element: <Levels /> },
+      { path: 'levels/:levelNumber', element: <Level /> },
+      { path: 'shop', element: <Shop /> },
+      { path: 'pokedex', element: <Pokedex /> },
+      { path: 'stats', element: <Stats /> },
+    ]
+  }
+])
+
+export default function App() {
+  return <RouterProvider router={router} />
+}
