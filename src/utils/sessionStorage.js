55be3e21dfec8e98b13f6a99c72b03ae7fabb16d
Annabel Wang <annabelwyn11@gmail.com>
2026-03-08 00:45:19 -0800

    state

--- src/utils/sessionStorage.js
+++ src/utils/sessionStorage.js
@@ -4,6 +4,18 @@ const DEFAULT_STATE = {
   pokemon: [],
   xp: 0,
   itemMultiplier: 1,
+  levelsUnlocked: 1,
+  stats: {
+    pokemonCaught: 0,
+    totalDamageDealt: 0,
+    // TODO: add more stats
+  }
+}
+
+const TEST_STATE = {
+  pokemon: [1, 4, 26, 69, 204],
+  xp: 0,
+  itemMultiplier: 1,
   levelsUnlocked: 2,
   stats: {
     pokemonCaught: 0,
@@ -14,7 +26,7 @@ const DEFAULT_STATE = {
 
 export function loadData() {
     const raw = sessionStorage.getItem(KEY)
-    return raw ? JSON.parse(raw) : DEFAULT_STATE
+    return raw ? JSON.parse(raw) : TEST_STATE
   }
   
   export function saveData(data) {
