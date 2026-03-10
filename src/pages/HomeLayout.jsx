157ca92b32ad4caa8b9655339de4eec7076d151a
notmeeko <micocbernardino@gmail.com>
2026-03-09 17:13:15 -0700

    added icons

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,6 +1,12 @@
 import { useOutletContext, NavLink, Link} from 'react-router-dom'
 import { useEffect } from 'react'
 
+import lockImage from '../assets/lock.png'
+import smileImage from '../assets/smile.png'
+import pokeballImage from '../assets/pokeball.svg'
+import shopImage from '../assets/shop.svg'
+import statsImage from '../assets/stats.svg'
+
 export default function HomeLayout() {
   const { playerData, updateData } = useOutletContext()
 
@@ -18,16 +24,41 @@ export default function HomeLayout() {
       </h1>
       <p className="font-quantico text-dark-gray mb-6">Collect pokémon and advance through the levels to beat the final boss!</p>
 
-      <div className="flex gap-4 flex-wrap mb-10">
+      <div className="text-2xl flex gap-4 flex-wrap mb-10">
         {Array.from({ length: 5 }, (_, i) => i + 1).map(level => {
           const unlocked = level <= playerData.levelsUnlocked
 
+          // Level 5
+          if (level === 5) {
+            if (unlocked) {
+              return (
+                <Link
+                  key={level}
+                  to={`/home/level/${level}`}
+                  className={`w-22 h-22 flex items-center justify-center rounded-lg shadow font-silkscreen bg-red-300 border-2 border-royal-blue hover:brightness-95`}
+                >
+                  {level}
+                </Link>
+              )
+            }
+
+            return (
+              <div
+                key={level}
+                className={`w-22 h-22 flex items-center justify-center rounded-lg shadow font-silkscreen bg-red-300 border-2 border-royal-blue`}
+              >
+                <img src={smileImage} alt="Lock Image" className="w-12 h-12" />
+              </div>
+            )
+          }
+
+          // Levels 1-4
           if (unlocked) {
             return (
               <Link
                 key={level}
                 to={`/home/level/${level}`}
-                className={`w-16 h-16 flex items-center justify-center rounded-lg shadow font-silkscreen bg-yellow border-2 border-royal-blue`}
+                className={`w-22 h-22 flex items-center justify-center rounded-lg shadow font-silkscreen bg-yellow border-2 border-royal-blue hover:brightness-95`}
               >
                 {level}
               </Link>
@@ -37,24 +68,27 @@ export default function HomeLayout() {
           return (
             <div
               key={level}
-              className="w-16 h-16 bg-powder-blue flex items-center justify-center rounded-lg opacity-60"
+              className="w-22 h-22 bg-powder-blue border-2 border-royal-blue flex items-center justify-center rounded-lg"
             >
-              🔒
+              <img src={lockImage} alt="Lock Image" className="w-12 h-12" />
             </div>
           )
         })}
       </div>
 
       <div className="flex gap-6 font-quantico text-lg">
-        <NavLink to="/home/pokedex" className="font-silkscreen text-royal-blue mb-4 hover:text-black bg-powder-blue border-2 border-royal-blue px-6 py-3 rounded-lg">
+        <NavLink to="/home/pokedex" className="font-silkscreen text-royal-blue mb-4 hover:text-black bg-powder-blue border-2 border-royal-blue px-6 py-3 rounded-lg flex items-center gap-3 hover:brightness-95">
+          <img src={pokeballImage} alt="Pokédex Image" className="w-6 h-6" />
           Pokédex
         </NavLink>
         
-        <NavLink to="/home/shop" className="font-silkscreen text-royal-blue mb-4 hover:text-black bg-powder-blue border-2 border-royal-blue px-6 py-3 rounded-lg">
+        <NavLink to="/home/shop" className="font-silkscreen text-royal-blue mb-4 hover:text-black bg-powder-blue border-2 border-royal-blue px-6 py-3 rounded-lg flex items-center gap-3 hover:brightness-95  ">
+          <img src={shopImage} alt="Shop Image" className="w-6 h-6" />
           Shop
         </NavLink>
 
-        <NavLink to="/home/stats" className="font-silkscreen text-royal-blue mb-4 hover:text-black bg-powder-blue border-2 border-royal-blue px-6 py-3 rounded-lg">
+        <NavLink to="/home/stats" className="font-silkscreen text-royal-blue mb-4 hover:text-black bg-powder-blue border-2 border-royal-blue px-6 py-3 rounded-lg flex items-center gap-3 hover:brightness-95">
+          <img src={statsImage} alt="Stats Image" className="w-6 h-6" />
           Stats
         </NavLink>
       </div>
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -8,47 +8,59 @@ export default function Stats() {
         <div className="border border-royal-blue inline-grid grid-cols-[10fr_2fr] max-w-3xl">
 
           {/* Row 1 */}
-          <div className="bg-white p-6 border-r border-b border-royal-blue">
-            <p className="text-xl font-quantico">Levels Earned</p>
-          </div>
-          <div className="bg-white p-6 text-right border-b border-royal-blue">
-            <p className="text-xl font-quantico">4/5</p>
+          <div className="contents group">
+            <div className="bg-white p-6 border-r border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">Levels Earned</p>
+            </div>
+            <div className="bg-white p-6 text-right border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">4/5</p>
+            </div>
           </div>
 
           {/* Row 2 */}
-          <div className="bg-white p-6 border-r border-b border-royal-blue">
-            <p className="text-xl font-quantico ">Total Clicks</p>
-          </div>
-          <div className="bg-white p-6 text-right border-b border-royal-blue">
-            <p className="text-xl font-quantico">676767676767</p>
-          </div>
-
-          <div className="bg-white p-6 border-r border-b border-royal-blue">
-            <p className="text-xl font-quantico">Total XP Earned</p>
-          </div>
-          <div className="bg-white p-6 text-right border-b border-royal-blue">
-            <p className="text-xl font-quantico">14000</p>
+          <div className="contents group">
+            <div className="bg-white p-6 border-r border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico ">Total Clicks</p>
+            </div>
+            <div className="bg-white p-6 text-right border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">676767676767</p>
+            </div>
           </div>
 
-          <div className="bg-white p-6 border-r border-b border-royal-blue">
-            <p className="text-xl font-quantico">Total XP Spent</p>
-          </div>
-          <div className="bg-white p-6 text-right border-b border-royal-blue">
-            <p className="text-xl font-quantico">200</p>
-          </div>
-
-          <div className="bg-white p-6 border-r border-b border-royal-blue">
-            <p className="text-xl font-quantico ">Pokemon Caught</p>
-          </div>
-          <div className="bg-white p-6 text-right border-b border-royal-blue">
-            <p className="text-xl font-quantico">17</p>
+          <div className="contents group">
+            <div className="bg-white p-6 border-r border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">Total XP Earned</p>
+            </div>
+            <div className="bg-white p-6 text-right border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">14000</p>
+            </div>
           </div>
 
-          <div className="bg-white p-6 border-r border-royal-blue">
-            <p className="text-xl font-quantico ">Wild Pokemon Fled</p>
+          <div className="contents group">
+            <div className="bg-white p-6 border-r border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">Total XP Spent</p>
+            </div>
+            <div className="bg-white p-6 text-right border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">200</p>
+            </div>
           </div>
-          <div className="bg-white p-6 text-right border-royal-blue">
-            <p className="text-xl font-quantico">38</p>
+
+          <div className="contents group">
+            <div className="bg-white p-6 border-r border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico ">Pokemon Caught</p>
+            </div>
+            <div className="bg-white p-6 text-right border-b border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">17</p>
+            </div>
+          </div>
+
+          <div className="contents group">
+            <div className="bg-white p-6 border-r border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico ">Wild Pokemon Fled</p>
+            </div>
+            <div className="bg-white p-6 text-right border-royal-blue group-hover:bg-yellow-200">
+              <p className="text-xl font-quantico">38</p>
+            </div>
           </div>
 
 
