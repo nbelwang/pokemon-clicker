87bb74cc7c53d8205f21619e0bbf22644a21b305
Dylan <dliuninja@gmail.com>
2026-03-12 11:03:12 -0700

    Revamped card styling for a more compact look.

--- src/components/pokedex/PokedexCard.jsx
+++ src/components/pokedex/PokedexCard.jsx
@@ -4,30 +4,26 @@ import { useOutletContext } from 'react-router-dom'
 export default function PokedexCard(props) {
   return (
     <div
-      className={`bg-white border border-powder-blue rounded-xl p-6 w-72 shadow ${props.onClick ? 'cursor-pointer hover:shadow-lg ' : ''}`}
+      className={`bg-white border border-powder-blue rounded-xl p-6 grow flex shadow ${props.onClick ? 'cursor-pointer hover:shadow-lg ' : ''}`}
       onClick={props.onClick}
     >
-        <div className="flex items-center justify-between mb-2">
-        <h2 className="font-press-start text-sm text-dark-gray uppercase">{props.name}</h2>
-        <span className={`font-quantico text-sm font-bold ${props.isCaught ? 'text-green' : 'text-salmon'}`}>
-            {props.isCaught ? 'Caught' : 'Not Caught'}
-        </span>
-        </div>
         <img
-        src={props.frontSprite}
-        alt={props.name}
-        className="w-35 h-35 mx-auto"
+          src={props.frontSprite}
+          alt={props.name}
+          className="w-24 h-24 mr-5 bg-powder-blue"
         />
-        <div className="mt-2 font-quantico text-sm flex flex-col gap-1">
-        <p>
-            <span className="font-bold">Type: </span>
-            {props.types.map(t => (
-            <span key={t.type.name} className="bg-slate-blue text-dark-gray px-2 py-0.5 rounded mr-1 text-xs">
-                {t.type.name}
-            </span>
-            ))}
-        </p>
-        <p><span className="font-bold">HP:</span> {props.stats[0].base_stat}</p>
+        <div className="font-quantico text-sm flex flex-col gap-1">
+          <h2 className="font-press-start text-sm text-dark-gray uppercase">{props.name}</h2>
+          <p><span className="font-bold">ATK:</span> {props.stats[1].base_stat}</p>
+          <p>
+              <span className="font-bold">Type: </span>
+              {props.types.map(t => (
+              <span key={t.type.name} className="bg-slate-blue text-dark-gray px-2 py-0.5 rounded mr-1 text-xs">
+                  {t.type.name}
+              </span>
+              ))}
+          </p>
+          <p><span className="font-bold">HP:</span> {props.stats[0].base_stat}</p>
         </div>
     </div>
   )
