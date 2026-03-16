109158c6234599f81b1943cf4e242b2850069c60
Katie Kimura <katiekimura2022@gmail.com>
2026-03-16 00:25:12 -0700

    Shop and Pokedex styling

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
-import PokedexSearch from '../components/pokedex/PokedexSearch'
-import PokedexDetail from '../components/pokedex/PokedexDetail'
+import { useState } from "react"
+import PokedexSearch from "../components/pokedex/PokedexSearch"
+import PokedexDetail from "../components/pokedex/PokedexDetail"
 
 export default function Pokedex() {
-  const [selectedPokemon, setSelectedPokemon] = useState(null)
+    const [selectedPokemon, setSelectedPokemon] = useState(null)
 
-  return (
-    <div className="flex flex-col lg:flex-row gap-5 p-5 h-[calc(100vh-100px)]">
-      {/* Just the normal search stuff */}
-      <PokedexSearch className="flex-1 min-w-0" onSelect={setSelectedPokemon} />
-      {/* Display PokedexDetail only if a Pokemon has been selected */}
-      <PokedexDetail className="flex-1 min-w-0" pokemon={selectedPokemon}/>
-    </div>
-  )
+    return (
+        <div className="h-full px-6 py-12 md:px-12 lg:px-24 pb-24">
+            <h1 className="font-silkscreen font-black text-3xl mb-6 md:text-4xl lg:text-5xl text-royal-blue">
+                POKÉDEX
+            </h1>
+            <div className="flex flex-col gap-y-12 md:grid md:gap-x-2 lg:gap-x-4 md:grid-cols-2">
+                <div className="flex-1">
+                    {/* Just the normal search stuff */}
+                    <PokedexSearch onSelect={setSelectedPokemon} />
+                </div>
+                <div className="flex-1">
+                    {/* Display PokedexDetail only if a Pokemon has been selected */}
+                    <PokedexDetail pokemon={selectedPokemon} />
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -84,15 +84,15 @@ export default function Shop() {
     }, [playerData])
 
     return (
-        <div className="h-lvh px-6 py-3 md:px-24 md:py-12 lg:px-48 lg:py-24">
-            <div className="flex flex-col gap-2 items-start md:flex-row md:justify-between md:items-center mb-6">
+        <div className="px-6 py-12 md:px-16 lg:px-24 mb-24">
+            <div className="flex flex-row justify-between items-center mb-6">
                 <h1 className="font-silkscreen font-black text-3xl mb-2 md:mb-0 md:text-4xl lg:text-5xl text-royal-blue">
                     Shop
                 </h1>
                 <XPDisplay xpData={playerData.xp} />
             </div>
             <div>
-                <p className="font-quantico text-lg md:text-xl lg:text-2xl mb-6">
+                <p className="font-quantico text-md md:text-lg lg:text-xl mb-6">
                     Purchase items with XP to multiply clicks! Items are applied
                     automatically once purchased. Note that multipliers do NOT
                     stack.
